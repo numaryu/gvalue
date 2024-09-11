8616ab9c6aaa8ba77a08c8161bb78113c14df92f
Ryusuke Numata <ryusuke.numata@gmail.com>
2024-09-11 17:45:59 +0900

    Edit comment

--- src/gvalue.f90
+++ src/gvalue.f90
@@ -3,6 +3,9 @@
 !
 !     Ionization and singlet and triplet excitations.
 !
+!     The original version (S. Sato, Radiation Chemistry 14, 2-19 (1979) [in Japanese])
+!     is written in Fortran 77, is rewritten by numaryu into Fortran 2023.
+!
 !     References
 !     1) S. Sato, K. Okazaki, S. Ohno, Bull. Chem. Soc. Jpn, 47, 2174 (1974).
 !     2) K. Okazaki, S. Sato, S. Ohno, Bull. Chem. Soc. Jpn, 48, 1411 (1975).
