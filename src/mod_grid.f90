ab675b47c678809204fc129c38dca439fc899fb6
Ryusuke Numata <ryusuke.numata@gmail.com>
2024-09-12 18:51:46 +0900

    Remove unused lines

--- src/mod_grid.f90
+++ src/mod_grid.f90
@@ -4,25 +4,12 @@ module mod_grid
   private
   public :: init_grid, finish_grid
 
-  public :: egrid_compat
-  public :: negrid_compat
-  public :: egrid_max_compat
-  public :: mesh_compat
-
   public :: egrid
   
   type(grid) :: egrid
   
   logical :: initialized = .false.
   logical :: debug = .false.
-  
-  ! values for backward compatibility
-  ! degrid_compat = (1/2)**(1/nediv) = T_(i+1)/T_(i) with nediv=40
-  real :: degrid_compat = 0.5**(1.0/40)
-  real :: egrid_max_compat = 1.e5, egrid_min_compat = 1.e-2
-  integer, parameter :: negrid_compat = 1500
-  
-  real :: egrid_compat(negrid_compat)
   
 contains
 
@@ -45,12 +32,6 @@ contains
 
     egrid = grid(nediv, egrid_max, egrid_min)
     
-    do i=1,negrid_compat
-       egrid_compat(i) = egrid_max_compat*degrid_compat**(i-1)
-    end do
-
-    write(6,*) 'grid diff = ',sum(abs(egrid_compat(1:min(negrid_compat,egrid%number))-egrid%val(1:min(negrid_compat,egrid%number))))
-    write(6,*) 'grid diff = ',negrid_compat,egrid%number
     initialized = .true.
   end subroutine init_grid
 
@@ -58,14 +39,5 @@ contains
     if (.not.initialized) return
     initialized = .false.
   end subroutine finish_grid
-
-  integer function mesh_compat(tx,tp)
-    !
-    !     Functtion of numbering electron energy
-    !
-    real, intent(in) :: tx, tp
-    mesh_compat = ifix(40.0*(alog(tp/tx)/alog(2.0))+1)
-    return
-  end function mesh_compat
   
 end module mod_grid
--- src/mod_orbital.f90
+++ src/mod_orbital.f90
@@ -7,29 +7,11 @@ module mod_orbital
 
   public :: medium
 
-  public :: norbital_compat
-  public :: energy_ionize_compat, energy_kinetic_compat
-  public :: energy_singlet_compat, energy_triplet_compat
-  public :: number_electrons_compat
-  public :: number_ionize_compat, gvalue_ionize_compat
-  public :: number_singlet_compat, gvalue_singlet_compat
-  public :: number_triplet_compat, gvalue_triplet_compat
-
   type(orbital) :: medium
-
-  integer :: norbital_compat
-  
-  real, allocatable :: energy_ionize_compat(:), energy_kinetic_compat(:)
-  real, allocatable :: energy_singlet_compat(:), energy_triplet_compat(:)
-  real, allocatable :: number_electrons_compat(:)
-
-  real, allocatable :: number_ionize_compat(:), gvalue_ionize_compat(:)
-  real, allocatable :: number_singlet_compat(:), gvalue_singlet_compat(:)
-  real, allocatable :: number_triplet_compat(:), gvalue_triplet_compat(:)
 
   logical :: initialized = .false.
   logical :: debug = .false.
-  
+
 contains
 
   subroutine init_orbital
@@ -60,43 +42,11 @@ contains
 
     medium%number_density = number_density
 
-    ! for backward compatibility
-    norbital_compat = norbital
-    if (.not.allocated(energy_ionize_compat)) allocate(energy_ionize_compat(norbital_compat))
-    if (.not.allocated(energy_kinetic_compat)) allocate(energy_kinetic_compat(norbital_compat))
-    if (.not.allocated(energy_singlet_compat)) allocate(energy_singlet_compat(norbital_compat))
-    if (.not.allocated(energy_triplet_compat)) allocate(energy_triplet_compat(norbital_compat))
-    if (.not.allocated(number_electrons_compat)) allocate(number_electrons_compat(norbital_compat))
-    energy_ionize_compat = medium%energy_ionize
-    energy_kinetic_compat = medium%energy_kinetic
-    energy_singlet_compat = medium%energy_singlet
-    energy_triplet_compat = medium%energy_triplet
-    number_electrons_compat = medium%number_electrons
-    
-    if (.not.allocated(number_ionize_compat)) allocate(number_ionize_compat(norbital_compat))
-    if (.not.allocated(gvalue_ionize_compat)) allocate(gvalue_ionize_compat(norbital_compat))
-    if (.not.allocated(number_singlet_compat)) allocate(number_singlet_compat(norbital_compat))
-    if (.not.allocated(gvalue_singlet_compat)) allocate(gvalue_singlet_compat(norbital_compat))
-    if (.not.allocated(number_triplet_compat)) allocate(number_triplet_compat(norbital_compat))
-    if (.not.allocated(gvalue_triplet_compat)) allocate(gvalue_triplet_compat(norbital_compat))
-    
     initialized = .true.
   end subroutine init_orbital
   
   subroutine finish_orbital
     if (.not.initialized) return
-    if (allocated(energy_ionize_compat)) deallocate(energy_ionize_compat)
-    if (allocated(energy_kinetic_compat)) deallocate(energy_kinetic_compat)
-    if (allocated(energy_singlet_compat)) deallocate(energy_singlet_compat)
-    if (allocated(energy_triplet_compat)) deallocate(energy_triplet_compat)
-    if (allocated(number_electrons_compat)) deallocate(number_electrons_compat)
-
-    if (allocated(number_ionize_compat)) deallocate(number_ionize_compat)
-    if (allocated(gvalue_ionize_compat)) deallocate(gvalue_ionize_compat)
-    if (allocated(number_singlet_compat)) deallocate(number_singlet_compat)
-    if (allocated(gvalue_singlet_compat)) deallocate(gvalue_singlet_compat)
-    if (allocated(number_triplet_compat)) deallocate(number_triplet_compat)
-    if (allocated(gvalue_triplet_compat)) deallocate(gvalue_triplet_compat)
     initialized = .false.
   end subroutine finish_orbital
   
