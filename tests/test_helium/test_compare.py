fb54da678ac2f4a026c51537b2176632dc16ff5c
Ryusuke Numata <ryusuke.numata@gmail.com>
2024-10-23 19:39:48 +0900

    Add degradation test

--- tests/test_helium/test_compare.py
+++ tests/test_helium/test_compare.py
@@ -1,52 +1,98 @@
 import pytest
 import numpy as np
+import scipy.interpolate as interpolate
 
-eps = 1.e-6
+eps = 1.e-5
 
 def test_stoppow():
     test_helium = np.loadtxt("test_helium.dat")
-    egrid_result = test_helium[:,0]
-    stoppow_result = test_helium[:,1]
-    print(stoppow_result[:10])
-    
+    egrid_result = np.flipud(test_helium[:, 0])
+    stoppow_result = np.flipud(test_helium[:, 1])
+    stoppow_result_interp = interpolate.CubicSpline(egrid_result, stoppow_result)
+
+    # minimum energy value that gives finite stoppow
+    energy_min_result = egrid_result[np.min(np.where(stoppow_result != 0))]
+
+    # data from WolframEngine
     stoppow = np.loadtxt("stoppow.dat")
-    egrid_soln = stoppow[:,1]
-    stoppow_soln = stoppow[:,3]
-    print(stoppow_soln[:10])
+    egrid_data = stoppow[:, 0]
+    stoppow_data = stoppow[:, 1]
+    stoppow_data_interp = interpolate.CubicSpline(egrid_data, stoppow_data)
 
-    rms = np.sqrt(np.average( (egrid_result - egrid_soln)**2) )
-    rms += np.sqrt(np.average( (stoppow_result - stoppow_soln)**2) )
+    # minimum energy value that gives finite stoppow
+    energy_min_data = egrid_data[np.min(np.where(stoppow_data != 0))]
 
-    print(rms)
+    # minimum valid energy
+    energy_min = max(energy_min_result, energy_min_data)
+
+    if egrid_result.size <= egrid_data.size:
+        egrid_check = egrid_result
+    else:
+        egrid_check = egrid_data
+
+    # mask if egrid is less than valid energy
+    madiff = np.ma.array((stoppow_result_interp(egrid_check) - \
+                          stoppow_data_interp(egrid_check))**2,
+                         mask = egrid_check < energy_min)
+    rms = np.ma.mean(madiff)
+
+    # print(rms)
     assert rms < eps
 
 def test_degradation():
+    ngen = 4
+
     test_helium = np.loadtxt("test_helium_degradation.dat")
-    egrid_result = test_helium[:,0]
-    ytot_result = test_helium[:,1]
-    y1_result = test_helium[:,2]
-    y2_result = test_helium[:,3]
-    y3_result = test_helium[:,4]
-    y4_result = test_helium[:,5]
-    y5_result = test_helium[:,6]
-    y6_result = test_helium[:,7]
-    print(y1_result[:10])
-    print(y2_result[100:110])
+    egrid_result = np.flipud(test_helium[:, 0])
+    deggen_result = np.flipud(test_helium[:, 2:2+ngen])
 
-    degradation = np.loadtxt("degradation320.dat")
-    egrid_soln = degradation[:,1]
-    y1_soln = degradation[:,2]
-    y2_soln = degradation[:,3]
+    # minimum energy value that gives finite stoppow
+    energy_min_result = np.zeros((ngen))
+    energy_max_result = np.zeros((ngen))
+    for i in range(ngen):
+        energy_min_result[i] = egrid_result[np.min(np.where(deggen_result[:, i] != 0))]
+        energy_max_result[i] = egrid_result[np.max(np.where(deggen_result[:, i] != 0))]
 
-    y1_spl = interpolate.interp1d(egrid_soln, y1_soln, kind="cubic")
-    y1_spl = interpolate.interp1d(egrid_soln, y1_soln, kind="cubic")
-    print(y1_soln[:10])
-    print(y2_soln[100:110])
+    # list of functions
+    deggen_result_interp = []
+    for i in range(ngen):
+        deggen_result_interp.append(interpolate.CubicSpline(egrid_result, deggen_result[:, i]))
 
-    rms1 = np.sqrt(np.average( (y1_result - y1_soln)**2) )
-    rms2 = np.sqrt(np.average( (y2_result - y2_soln)**2) )
+    # data from WolframEngine
+    degradation = np.loadtxt("degradation.dat")
+    egrid_data = degradation[:, 0]
+    deggen_data = degradation[:, 1:1+ngen]
 
-    print(rms1)
-    print(rms2)
-    assert rms1 < eps
-    assert rms2 < eps
+    # minimum energy value that gives finite stoppow
+    energy_min_data = np.zeros((ngen))
+    energy_max_data = np.zeros((ngen))
+    for i in range(ngen):
+        energy_min_data[i] = egrid_data[np.min(np.where(deggen_data[:, i] != 0))]
+        energy_max_data[i] = egrid_data[np.max(np.where(deggen_data[:, i] != 0))]
+
+    # list of functions
+    deggen_data_interp = []
+    for i in range(ngen):
+        deggen_data_interp.append(interpolate.CubicSpline(egrid_data, deggen_data[:, i]))
+
+    # minimum valid energy
+    energy_min = np.max(np.stack([energy_min_result, energy_min_data]), axis = 0)
+    energy_max = np.min(np.stack([energy_max_result, energy_max_data]), axis = 0)
+
+    if egrid_result.size <= egrid_data.size:
+        egrid_check = egrid_result
+    else:
+        egrid_check = egrid_data
+
+    rms = []
+    for i in range(ngen):
+        # mask if egrid is not in valid energy range
+        madiff = np.ma.array((deggen_result_interp[i](egrid_check) - \
+                              deggen_data_interp[i](egrid_check))**2,
+                             mask = np.any([egrid_check < energy_min[i],
+                                            egrid_check > energy_max[i]], axis=0))
+        rms.append(np.ma.mean(madiff))
+
+    # print(rms)
+    for i in range(ngen):
+        assert rms[i] < eps
