87296858175cec21ee150ba74243efebc7fde3f0
Ryusuke Numata <ryusuke.numata@gmail.com>
2025-01-16 20:01:01 +0900

    Update mixture test
    also check spectra

--- tests/test_mixture/test_compare.py
+++ tests/test_mixture/test_compare.py
@@ -1,7 +1,17 @@
 import pytest
 import numpy as np
 
+eps_spectra = 1.e-6
 eps_gvalue = 1.e-4
+
+def test_spectra():
+    spectra_result = np.loadtxt("test_mixture.dat")
+    spectra_ref = np.loadtxt("test_mixture_ref.dat")
+
+    rms = np.sqrt(np.mean((spectra_result-spectra_ref)**2))
+
+    # print(rms)
+    assert rms < eps_spectra
 
 def test_gvalue():
     gvalue_result = np.loadtxt("test_mixture_gvalue.dat")
